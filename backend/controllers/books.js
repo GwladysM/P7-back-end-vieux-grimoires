fc91f7bff06a6971fa3bd2c60bcb4935b0198c85
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-09-07 17:04:50 -0400

    creation routes : rateABook et bestRatings

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,6 +1,7 @@
 const Book = require('../models/Book');
 const fs = require('fs');
 
+//Ajout d'un nouveau livre :
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
@@ -16,6 +17,7 @@ exports.createBook = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
+//Modification d'un livre :
 exports.modifyBook = (req, res, next) => {
     const bookObject = req.file ? {
         ...JSON.parse(req.body.book),
@@ -39,6 +41,8 @@ exports.modifyBook = (req, res, next) => {
         });
 };
 
+
+//Suppression d'un livre :
 exports.deleteBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
         .then(book => {
@@ -56,14 +60,62 @@ exports.deleteBook = (req, res, next) => {
         .catch(error => { res.status(500).json({ error }) });
 };
 
+//Afficher un livre :
 exports.getOneBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
         .then(book => res.status(200).json(book))
         .catch(error => res.status(404).json({ error }))
 };
 
+//Afficher la liste des livres :
 exports.getAllBooks = (req, res, next) => {
     Book.find()
         .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
+};
+
+// Ajout d'une note à un livre :
+exports.rateABook = (req, res, next) => {
+    const grade = parseFloat(req.body.rating);
+
+    Book.findOne({ _id: req.params.id })
+        .then(book => {
+            if (!book) {
+                throw new Error("Le livre n'a pas été trouvé");
+            }
+
+            //Vérifier que l'utilisateur n'a pas déjà noté le livre :
+            const userAlreadyRate = book.ratings.find(rating => rating.userId.toString() === req.auth.userId);
+            if (userAlreadyRate) {
+                throw new Error('Note déjà attribuée')
+            }
+
+            //Ajout de la nouvelle note :
+            book.ratings.push({
+                userId: req.auth.userId,
+                grade
+            });
+
+            //Total des notes et moyenne du livre :
+            const allRatings = book.ratings.reduce((acc, curr) => acc + curr.grade, 0);
+            book.averageRating = allRatings / book.ratings.length;
+
+            return book.save();
+        })
+        .then(() => res.status(201).json({ message: 'Note ajoutée !' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+// Affichage des 3 meilleurs livres :
+exports.bestRating = (req, res) => {
+    Book.find()
+        .then(books => {
+            //Ordonner les livres par note croissante :
+            books.sort((a, b) => b.averageRating - a.averageRating);
+
+            //Choix des 3 premiers livres :
+            const bestBooks = books.slice(0, 3);
+            res.status(200).json(bestBooks)
+        })
         .catch(error => res.status(400).json({ error }));
 };
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -9,29 +9,19 @@ const bookCtrl = require('../controllers/books');
 // Création d'un nouveau livre :
 router.post('/', auth, multer, bookCtrl.createBook);
 
-// Affichage de la note du livre :
-router.post('/:id/rating', (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({
-        message: 'Défini la note pour le user ID fourni'
-    });
-});
-
-// Affichage de la liste des livres :
-router.get('/', bookCtrl.getAllBooks);
+// Ajout d'une note à un livre :
+router.post('/:id/rating', auth, bookCtrl.rateABook);
 
 // Affichage d'un livre :
 router.get('/:id', bookCtrl.getOneBook);
 
-// Affichage des 3 meilleurs livres :
-router.get('/bestrating', (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({
-        message: 'liste des meilleurs livres à créer !'
-    });
-});
+// Affichage de la liste des livres :
+router.get('/', bookCtrl.getAllBooks);
 
-// Mise à jour du livre :
+// Affichage des 3 meilleurs livres :
+router.get('/bestrating', bookCtrl.bestRating);
+
+// Modifier un livre :
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 
 // Suppression d'un livre :
