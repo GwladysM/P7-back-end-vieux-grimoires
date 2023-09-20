7c544f6687e49ad9fc60f3f01ff4883c6e44b751
Gwladys MORLIER <gwladys.morlier@hotmail.fr>
2023-09-20 10:11:26 -0400

    simplification appel callback

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,22 +1,14 @@
 const multer = require('multer');
 const SharpMulter = require('sharp-multer');
 
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png'
-};
-
 const storage = SharpMulter({
     destination: (req, file, callback) => callback(null, "images"),
     imageOptions: {
         fileFormat: "webp",
-        quality: 30,
+        quality: 50,
         resize: { width: 400, height: 500 },
         filename: (req, file, callback) => {
-            const timestamp = new Date().toISOString();
-            const ref = `${timestamp}-${file.originalname}.webp`
-            callback(null, ref);
+            callback(null, file.originalname + Date.now() + '.webp');
         }
     }
 });
